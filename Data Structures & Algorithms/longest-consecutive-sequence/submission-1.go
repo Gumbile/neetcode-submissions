6f689d87fca052e08ac2effb7b2147c38aa0f8ab
Abdelrahman mohamed <108292349+Gumbile@users.noreply.github.com>
2026-04-23 10:47:59 +0200

    Add: longest-consecutive-sequence - submission-1

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-1.go
@@ -0,0 +1,63 @@
+func longestConsecutive(nums []int) int {
+
+	shiftBy := findMin(nums)
+
+	if shiftBy >= 0 {
+		shiftBy = 0
+	}else {
+		shiftBy = -1 * shiftBy
+	}
+
+	set := make(map[int]struct{})
+
+	for _,num := range nums {
+		set[num + shiftBy] = struct{}{}
+	} 
+
+	run := 0
+
+	for _,num := range nums{
+		
+		newHash := num + shiftBy
+		
+		if _,ok := set[newHash - 1]; !ok {
+			run = max(calcRun(set,newHash),run)
+		}
+	}
+
+	return run
+
+
+}
+
+
+func calcRun(set map[int]struct{} , numShifted int) int {
+	
+	run := 1
+	i := 1
+	
+	for {
+		
+		if _,ok := set[numShifted + i]; ok {
+			run++
+			i++
+		
+		}else{
+			return run
+		}
+	}
+}
+
+
+func findMin(nums []int) int {
+	if len(nums) == 0 {
+		return 0 
+	}
+	min := nums[0]
+	for _, v := range nums {
+		if v < min {
+			min = v
+		}
+	}
+	return min
+}
