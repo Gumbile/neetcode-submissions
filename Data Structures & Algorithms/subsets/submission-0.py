dd64a31fc591b80a476842685dcca2e08d87dd76
Abdelrahman mohamed <108292349+Gumbile@users.noreply.github.com>
2026-07-03 12:05:04 +0300

    Add: subsets - submission-0

---
+++ Data Structures & Algorithms/subsets/submission-0.py
@@ -0,0 +1,16 @@
+import copy
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        
+        res = [[]]
+        def rec(numToAdd,arr:list[int]):
+
+            for i,num in enumerate(numToAdd):
+                arr.append(num)
+                res.append(copy.deepcopy(arr))
+                rec(numToAdd[i+1:],arr)
+                arr.pop()
+
+        rec(nums,[])
+
+        return res
